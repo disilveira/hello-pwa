c9d33edcaf2ef0f65cf1a5f06446f99e4c023e4e
Diego Almeida <diego.almeida@outlook.com>
2020-02-06 15:09:39 -0300

    Added splash screen when app start

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <script src="js/jquery-3.4.1.min.js"></script>
-<script src="js/main.js"></script>
+<script src="app.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
--- head.php
+++ head.php
@@ -1,20 +1,25 @@
 <head>
+    <title>PWA App</title>    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
-    <title>PWA App</title>
     <link rel="manifest" href="manifest.json">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff">
+    <meta name="apple-mobile-web-app-title" content="PWA App">
+    <meta name="theme-color" content="#ffffff">
     <link rel="apple-touch-icon" href="touch-icon-iphone.png">
     <link rel="apple-touch-icon" sizes="152x152" href="images/icons/icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="images/icons/icon-180x180.png">
     <link rel="apple-touch-icon" sizes="167x167" href="images/icons/icon-167x167.png">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <link href="images/icons/icon-512x512.png" sizes="2048x2732" rel="apple-touch-startup-image" />
-    <link href="images/icons/icon-512x512.png" sizes="1668x2224" rel="apple-touch-startup-image" />
-    <link href="images/icons/icon-512x512.png" sizes="1536x2048" rel="apple-touch-startup-image" />
-    <link href="images/icons/icon-512x512.png" sizes="1125x2436" rel="apple-touch-startup-image" />
-    <link href="images/icons/icon-512x512.png" sizes="1242x2208" rel="apple-touch-startup-image" />
-    <link href="images/icons/icon-512x512.png" sizes="750x1334" rel="apple-touch-startup-image" />
-    <link href="images/icons/icon-512x512.png" sizes="640x1136" rel="apple-touch-startup-image" />
+    <link rel="apple-touch-startup-image" href="images/splash/launch-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+    <link rel="apple-touch-startup-image" href="images/splash/launch-750x1294.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+    <link rel="apple-touch-startup-image" href="images/splash/launch-1242x2148.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+    <link rel="apple-touch-startup-image" href="images/splash/launch-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+    <link rel="apple-touch-startup-image" href="images/splash/launch-1536x2048.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+    <link rel="apple-touch-startup-image" href="images/splash/launch-1668x2224.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+    <link rel="apple-touch-startup-image" href="images/splash/launch-2048x2732.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+    
     <link rel="shortcut icon" href="favicon.ico" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
